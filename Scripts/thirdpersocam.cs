3175dbbfa250c662638e3aef6375f251245715d4
Andoni Garcia <47414947+andonigl14@users.noreply.github.com>
2019-02-26 16:57:09 +0100

    Add files via upload

---
+++ Scripts/thirdpersocam.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class thirdpersocam : MonoBehaviour  {
+
+    public GameObject target;
+    public Vector3 offset = new Vector3(10,10,0);
+		
+	//we use lateupdate cause its called after all other update funtions so the camera follows just after the player movement.
+	void LateUpdate ()
+    {
+        transform.position = target.transform.position + offset;
+	}
+}
